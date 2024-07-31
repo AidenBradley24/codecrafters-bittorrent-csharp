d9c681caa8e09448193893a8e6be87d55012fd23
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-07-30 20:01:38 -0400

    refactor and bencoded int

---
+++ src/Bencode.cs
@@ -0,0 +1,41 @@
+﻿namespace BitTorrentFeatures
+{
+    public static class Bencode
+    {
+        public static object Decode(string encodedValue)
+        {
+            char first = encodedValue[0];
+            if (char.IsDigit(first))
+            {
+                // string
+                int colonIndex = encodedValue.IndexOf(':');
+                if (colonIndex != -1)
+                {
+                    int length = int.Parse(encodedValue[..colonIndex]);
+                    string value = encodedValue.Substring(colonIndex + 1, length);
+                    return value;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+                }
+            }
+            else if (first == 'i')
+            {
+                // integer
+                int endIndex = encodedValue.IndexOf('e');
+                if (endIndex != -1)
+                {
+                    int value = int.Parse(encodedValue[..endIndex]);
+                    return value;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+                }
+            }
+
+            throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using BitTorrentFeatures;
 using System.Text.Json;
 
 // Parse arguments
@@ -11,27 +12,7 @@ var (command, param) = args.Length switch
 // Parse command and act accordingly
 if (command == "decode")
 {
-    // Uncomment this line to pass the first stage
-    var encodedValue = param;
-    if (Char.IsDigit(encodedValue[0]))
-    {
-        // Example: "5:hello" -> "hello"
-        var colonIndex = encodedValue.IndexOf(':');
-        if (colonIndex != -1)
-        {
-            var strLength = int.Parse(encodedValue[..colonIndex]);
-            var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-            Console.WriteLine(JsonSerializer.Serialize(strValue));
-        }
-        else
-        {
-            throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-        }
-    }
-    else
-    {
-        throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-    }
+    Console.WriteLine(JsonSerializer.Serialize(Bencode.Decode(param)));
 }
 else
 {
