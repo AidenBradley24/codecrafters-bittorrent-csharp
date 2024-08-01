2ef56417f1521631f74685c0313cbee792649cbb
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-08-01 16:01:42 -0400

    fix

---
+++ src/MiscUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace BitTorrentFeatures
+{
+    public static class MiscUtils
+    {
+        public static string HashHex(byte[] hash)
+        {
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+
+        public static byte[] BigEndian(ReadOnlySpan<byte> span)
+        {
+            byte[] result = span.ToArray();
+            if (BitConverter.IsLittleEndian) Array.Reverse(result);
+            return result;
+        }
+    }
+}
