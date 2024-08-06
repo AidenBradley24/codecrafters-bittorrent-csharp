5bfddcd3c06346d774d8e991e5e818d232623664
Aiden Bradley <116838707+AidenBradley24@users.noreply.github.com>
2024-08-06 19:41:52 -0400

    test

--- src/TorrentClient.cs
+++ src/TorrentClient.cs
@@ -18,9 +18,7 @@ namespace BitTorrentFeatures
             Tor = torrent;
             PeerID = peerID;
             ns = tcp.GetStream();
-            var task = PeerMessage.RecieveAsync(ns);
-            task.Wait();
-            var message = task.Result;
+            var message = PeerMessage.Recieve(ns);
             if (message.Type != PeerMessage.Id.Bitfield) throw new Exception("not a bitfield");
             PeerMessage.Send(ns, PeerMessage.Id.Interested, null);
             message = PeerMessage.Recieve(ns);
